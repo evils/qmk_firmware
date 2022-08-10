25e16d813df6ef70c8e7bb98f606469c458a928f
Evils <evils.devils@protonmail.com>
2022-08-10 22:35:06 +0200

    picopad: use latest keymap

--- keyboards/handwired/picopad/keymaps/via/keymap.c
+++ keyboards/handwired/picopad/keymaps/via/keymap.c
@@ -13,21 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_3x3(
         KC_VOLD,   KC_MUTE,   KC_VOLU,
         KC_LEFT,   KC_MPLY,   KC_RGHT,
-        KC_PSCR,   KC_INS,   MO(1)
+        KC_PSCR,   KC_INS,    MO(1)
     ),
     [1] = LAYOUT_ortho_3x3(
         QK_BOOT,   DB_TOGG,   MAGIC_TOGGLE_NKRO,
-        EE_CLR,   KC_UP,   RGB_TOG,
-        MO(2),   KC_DOWN,   KC_TRNS
-    ),
-    [2] = LAYOUT_ortho_3x3(
-        KC_TRNS,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS,   MO(3)
-    ),
-    [3] = LAYOUT_ortho_3x3(
-        KC_TRNS,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,   KC_TRNS,   KC_TRNS
+        RGB_MOD,   RGB_TOG,   RGB_RMOD,
+        MO(2),     EE_CLR,    KC_TRNS
     )
 };
