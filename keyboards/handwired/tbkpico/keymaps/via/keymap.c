57e2d7f8abfd7e61534da8316a23427a60543fae
Evils <evils.devils@protonmail.com>
2023-03-21 05:56:08 +0100

    tbkpico: update via keymap

--- keyboards/handwired/tbkpico/keymaps/via/keymap.c
+++ keyboards/handwired/tbkpico/keymaps/via/keymap.c
@@ -16,25 +16,25 @@ OSM(MOD_LCTL),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_LBRC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RBRC,
+  KC_GRV, S(KC_GRV), S(KC_9), S(KC_BSLS), S(KC_0), KC_BSLS,                      KC_MINS,    KC_1,    KC_4,    KC_7,    KC_0,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      OSM(MOD_LSFT), OSM(MOD_LALT), KC_VOLD, KC_UP, KC_VOLU, KC_PSCR,            KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+OSM(MOD_LSFT), OSM(MOD_LALT), KC_VOLD, KC_UP, KC_VOLU, KC_PSCR,                  S(KC_8),    KC_2,   KC_5,  KC_8, S(KC_RBRC), KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      OSM(MOD_LCTL), KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY,                KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+OSM(MOD_LCTL), KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY,                         KC_E,    KC_3,    KC_6,    KC_9, KC_LBRC, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LSFT,  OSL(2),   MO(1),    KC_RGUI,  KC_ENT, KC_BSPC
+                                          KC_LSFT,  OSL(2),   MO(1),    KC_TRNS, KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_HOME,   KC_F1,   KC_F4,   KC_F7,  KC_F10, KC_PGUP,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_CAPS,KC_NO,LCTL(LSFT(KC_TAB)),KC_NO,LCTL(KC_TAB),KC_NO,                    KC_NO,  KC_INS, KC_HOME, KC_PGUP, KC_RSFT, KC_PSCR,
+ KC_CAPS, KC_NO, C(S(KC_TAB)), KC_MS_UP, C(KC_TAB),  KC_NO,                       KC_END,   KC_F2,   KC_F5,   KC_F8,  KC_F11, KC_PGDN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        DF(0),   DF(4),   KC_NO,   KC_NO,   KC_NO, KC_MPLY,                        KC_NO,  KC_DEL,  KC_END, KC_PGDN, KC_RCTL, KC_RALT,
+DF(0), DF(4), KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MPLY,                         KC_W,   KC_F3,   KC_F6,   KC_F9,  KC_F12, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LSFT,   MO(2),  OSL(3),     KC_TRNS, KC_TRNS, KC_TRNS
+                                          KC_LSFT,   MO(2),  OSL(3),     KC_TRNS, KC_INS,  KC_DEL
                                       //`--------------------------'  `--------------------------'
   ),
 
